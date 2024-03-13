75e937978f82db283490ce4263305226c64e4e43
psbrunosouza <psbrunosouza@gmail.com>
2024-03-13 19:11:16 -0300

    feat: implement responsivity

--- src/app/dashboard-dengue/dashboard-dengue.component.ts
+++ src/app/dashboard-dengue/dashboard-dengue.component.ts
@@ -1,10 +1,4 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  ViewChild,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ByGenreComponent } from './ui/by-genre/by-genre.component';
 import { ByRaceComponent } from './ui/by-race/by-race.component';
 import { MapComponent } from './ui/map/map.component';
@@ -17,21 +11,4 @@ import { SeLevelsComponent } from './ui/se-levels/se-levels.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [MapComponent, SeLevelsComponent, ByGenreComponent, ByRaceComponent],
 })
-export class DashboardDengueComponent implements AfterViewInit {
-  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
-
-  ngAfterViewInit(): void {
-    const rect = this.canvas.nativeElement.getBoundingClientRect();
-
-    console.log(rect);
-
-    var ctx = this.canvas.nativeElement.getContext('2d');
-    if (ctx) {
-      ctx.strokeStyle = 'red';
-      ctx.beginPath();
-      ctx.moveTo(0, 130);
-      ctx.lineTo(200, 130);
-      ctx.stroke();
-    }
-  }
-}
+export class DashboardDengueComponent {}
