8cdd0d733dee4b96ed14641b6874d7265349a8fe
psbrunosouza <psbrunosouza@gmail.com>
2024-03-15 13:04:27 -0300

    feat: add proxy cors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IconsModule } from '../modules/icons/icons.module';
+import { DashboardDengueApiService } from '../services/dashboard-dengue/dashboard-dengue-api.service';
 import { CardComponent } from './ui/card/card.component';
 import { MapComponent } from './ui/map/map.component';
 
@@ -9,8 +11,33 @@ import { MapComponent } from './ui/map/map.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, MapComponent, CardComponent, IconsModule],
+  imports: [
+    RouterOutlet,
+    MapComponent,
+    CardComponent,
+    IconsModule,
+    HttpClientModule,
+  ],
+  providers: [DashboardDengueApiService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dengue-map';
+
+  constructor(private dashboardDengueApiService: DashboardDengueApiService) {}
+
+  ngOnInit(): void {
+    this.listDiseaseData();
+  }
+
+  listDiseaseData() {
+    this.dashboardDengueApiService
+      .getDiseasesData({
+        geocode: '2977408',
+        disease: 'dengue',
+        format: 'json',
+      })
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
--- src/services/dashboard-dengue/dashboard-dengue-api.service.ts
+++ src/services/dashboard-dengue/dashboard-dengue-api.service.ts
@@ -1,22 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Disease } from '../../interfaces/disease';
 
-interface DiseaseFilter {
+export interface DiseaseFilter {
   geocode: string;
   disease: string;
+  format: string;
+  ew_start?: number;
+  ew_end?: number;
+  ey_start?: number;
+  ey_end?: number;
 }
 
 @Injectable()
 export class DashboardDengueApiService {
+  resource: string = 'https://info.dengue.mat.br/api/alertcity';
+
   constructor(private httpClient: HttpClient) {}
 
-  getDiseasesData(diseaseFilter: any): Observable<any> {
-    return this.httpClient.get<Disease>(
-      'https://info.dengue.mat.br/api/alertcity',
+  getDiseasesData(diseaseFilter: DiseaseFilter): Observable<any> {
+    return this.httpClient.get(
+      `${this.resource}?format=${diseaseFilter.format}&geocode=${diseaseFilter.geocode}&disease=${diseaseFilter.disease}`,
       {
-        params: diseaseFilter,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }
     );
   }
